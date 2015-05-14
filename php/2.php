18caa5a91318964eebda15e599c07115ac2d59ce
huynt <huynt57@gmail.com>
2015-05-14 09:49:01 +0700

    my work in first week

---
+++ php/2.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Savvycom training - Javascript 1</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+
+        <!-- Latest compiled and minified JavaScript -->
+
+        <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+        <script>
+            $(document).ready(function() {
+
+
+            });
+        </script>
+    </head>
+
+
+    <body>
+
+        <div class="row">
+            <div class="col-lg-1">
+
+
+            </div><!-- /.col-lg-6 -->
+
+
+            <div  class="col-lg-2" id="result" >
+                <?php
+                for ($i = 'a'; $i < 'z'; $i++)
+                    echo "$i\n";
+                echo "z";
+                ?>
+
+            </div>
+        </div><!-- /.row -->
+    </body>
+</html>
+
